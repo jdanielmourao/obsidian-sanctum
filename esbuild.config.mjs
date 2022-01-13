691592088502e5b3ecad2a9a50649dd96cc6ce33
jdanielmourao <jdanielmourao@gmail.com>
2022-01-13 18:11:44 +0000

    reorganization and workflow optimization

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -2,6 +2,7 @@ import esbuild from "esbuild";
 import { sassPlugin } from "esbuild-sass-plugin";
 import process from "process";
 import { config } from "dotenv";
+import { readFileSync } from "fs";
 
 config();
 
@@ -12,64 +13,34 @@ const prod = process.argv[2] === "production";
  * npm run build will place it in the build directory in this repository.
  */
 const dir = prod ? "./build" : process.env.OUTDIR;
-const file = `${dir}/beta.css`;
-const banner = `/* ─────────────────────────────────────────────────────────────────────────
-Sanctum for Obsidian.md
-Version 0.6.0
-created by @jdanielmourao (Github)
+const file = `${dir}/Sanctum.css`;
 
-Sponsor my work:
-https://ko-fi.com/jdanielmourao
-https://paypal.me/jdanielmourao
-
-Readme:
-https://github.com/jdanielmourao/obsidian-sanctum
-
-Forum Thread:
-https://forum.obsidian.md/t/sanctum-theme/25455
-
-Description:
-Sanctum’s a minimalist theme with the aim of creating a serene space of retreat, for thought and uninterrupted work.
-
-Disclaimer:
-This theme is designed to be used with the Style Settings plugin.
-
-MIT License
-
-Copyright (c) 2021-2022 José Daniel Mourão (jdanielmourao)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-───────────────────────────────────────────────────────────────────────── */`;
+/**
+ * readFileSync reads the file data in as a string.
+ */
+const banner = readFileSync("./scss/abstracts/_version.scss", { encoding: "utf8" });
+const footer = readFileSync("./scss/extensions/_style-settings.scss", {
+	encoding: "utf8"
+});
 
 esbuild
-    .build({
+	.build({
+		/** Entry point should be where everything is imported into. */
+		entryPoints: ["scss/base.scss"],
+		/** npm run dev will watch for file changes and rebuild instantly. */
+		watch: !prod,
+		/** Banner places the content at the beginning of the bundled file. */
 		banner: {
 			css: banner
 		},
-        /** Entry point should be where everything is imported into. */
-        entryPoints: ["scss/base.scss"],
-        /** npm run dev will watch for file changes and rebuild instantly. */
-        watch: !prod,
-		minify: false,
-        logLevel: "info",
-        plugins: [sassPlugin()],
-        outfile: file
-    })
-    .catch(() => process.exit(1));
+		/** Footer places the content at the end of the bundled file. */
+		footer: {
+			css: footer
+		},
+		logLevel: "info",
+		bundle: true,
+		minify: true,
+		plugins: [sassPlugin()],
+		outfile: file
+	})
+	.catch(() => process.exit(1));
