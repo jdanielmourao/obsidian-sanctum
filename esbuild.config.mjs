7c9b64ca7c73a649b716452a243f80781c49903b
jdanielmourao <jdanielmourao@gmail.com>
2022-09-02 00:19:21 +0100

    1.0.6

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -4,9 +4,11 @@
  */
 
 import esbuild from "esbuild";
+import process from "process";
 import { config } from "dotenv";
 import { sassPlugin } from "esbuild-sass-plugin";
 import time from "esbuild-plugin-time";
+import { readFileSync } from "fs";
 
 config();
 
@@ -22,11 +24,23 @@ const dir = prod ? "./" : process.env.OUTDIR;
 const fileProd = `${dir}/theme.css`;
 const fileDev = `${dir}/SanctumDev.css`
 
+/** readFileSync reads the file data in a string. */
+const license = readFileSync("./src/css/license.css", "utf8");
+const hub = readFileSync("./src/css/plugin-compatibility.css", "utf8");
+
 esbuild
 	.build({
 		/** Entry point should be where everything is imported into. */
 		entryPoints: ["src/scss/index.scss"],
 		
+		/** Banner places the content at the beginning of the bundled file. */
+		banner: {
+			css: license
+		},
+		/** Footer places the content at the end of the bundled file. */
+		footer: {
+			css: hub
+		},
 		/** npm run dev will watch for file changes and rebuild instantly. */
 		watch: !prod,
 		logLevel: "info",
